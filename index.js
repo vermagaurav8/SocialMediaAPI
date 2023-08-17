1b60b558b01d0a8bab107aa55eba165f24169646
Gaurav Verma <gaurav.verma382001@gmail.com>
2023-08-17 21:27:48 +0530

    basic Setup & DB Connection

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const app = require('express')();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-    res.json({message: 'Docker is easy'});
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect Database
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "socialApiDB",
+}).then(() => {
+    console.log("DB Connected");
+}).catch((err) => {
+    console.log(err);
 })
 
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`running on port ${port}`));
+app.listen(port);
